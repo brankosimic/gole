3d246af0662bfdb916dec063181cc9699621ebcc
Branko Simic <branko.simic.sarajevo@gmail.com>
2024-01-18 21:34:07 +0100

    aaaa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import homeImg from '/public/home.jpg'
-import Hero from "@/components/hero"
+// import homeImg from '/public/home.jpg'
+// import Hero from "@/components/hero"
 
 export default function Home() {
-
-  return (
-    <Hero imgData={homeImg} imgAlt="Car factory" title="Professional Cloud Hosting" />
-  )
+  return <div />
+  // return (
+  //   <Hero imgData={homeImg} imgAlt="Car factory" title="Professional Cloud Hosting" />
+  // )
 }
